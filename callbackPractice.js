6da56c728adccea543517d07809291eff354651c
joshwilcken <joshuawilcken@Joshuas-MacBook-Pro.local>
2017-05-05 17:01:25 -0600

    adding callbackpractice

--- callbackPractice.js
+++ callbackPractice.js
@@ -22,7 +22,10 @@ and what you should write is the sayHi function that makes the code above work,
 
 // 1. Write a function called first that returns the first item of the array using a callback function
 
-  // Code Here
+function first(arr, cb) {
+  return cb (arr[0]);
+   
+}
 
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -32,9 +35,13 @@ first(names, function(firstName){
 
 
 
+
+
 // 2. Write a function called last which returns the last item of the array using a callback function.
 
-  //Code Here
+  function last (arr, cb) {
+    return cb (arr[arr.length-1])
+  }
 
 last(names, function(lastName){
   console.log('The last name in names is ' + lastName);
@@ -44,8 +51,10 @@ last(names, function(lastName){
 
 // 3. Write a function called multiply that multiplies two numbers using a callback function.
 
-  //Code Here
-
+function multiply(num1, num2, cb) {
+  var product = num1 * num2;
+  return cb (product);
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
@@ -56,7 +65,16 @@ multiply(4, 3, function(answer){
 // 4. Write a function called contains that checks if a name exists in an array. 
 // If it does, return true using the callback, if not return false.
 
-  //Code Here 
+function contains(arr, name, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === name) {
+      return cb(true)
+  } 
+    else {
+      return cb(false);
+    }
+  }
+} //Code Here 
 
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -71,7 +89,9 @@ contains(names, 'Colt', function(result){
 // 5. Write a function called uniq that takes the names array and removes all duplicates and returns 
 // the callback function with the array of unique names.
 
-    //Code Here
+ function uniq(arr, person, cb) {
+   
+ }
 
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
